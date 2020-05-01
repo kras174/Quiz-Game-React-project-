e52ead9f5c1a709599960ef787bd14f26c9a3fb7
kras174 <kras32a@gmail.com>
2020-05-02 02:58:25 +0500

    another small fix

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -19,21 +19,28 @@ class Quiz extends Component {
         return (
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
-                    <h1>Ответьте на вопросы</h1>
-
                     {this.props.loading || !this.props.quiz ? (
-                        <Loader />
+                        <>
+                            <h1>Загрузка...</h1>
+                            <Loader />
+                        </>
                     ) : this.props.isFinished ? (
-                        <FinishedQuiz results={this.props.results} quiz={this.props.quiz} onRetry={this.props.quizRetry} />
+                        <>
+                            <h1>Результаты</h1>
+                            <FinishedQuiz results={this.props.results} quiz={this.props.quiz} onRetry={this.props.quizRetry} />
+                        </>
                     ) : (
-                        <ActiveQuiz
-                            answers={this.props.quiz[this.props.activeQuestion].answers}
-                            question={this.props.quiz[this.props.activeQuestion].question}
-                            onAnswerClick={this.props.quizAnswerClick}
-                            quizLength={this.props.quiz.length}
-                            answerNumber={this.props.activeQuestion + 1}
-                            state={this.props.answerState}
-                        />
+                        <>
+                            <h1>Ответьте на вопросы</h1>
+                            <ActiveQuiz
+                                answers={this.props.quiz[this.props.activeQuestion].answers}
+                                question={this.props.quiz[this.props.activeQuestion].question}
+                                onAnswerClick={this.props.quizAnswerClick}
+                                quizLength={this.props.quiz.length}
+                                answerNumber={this.props.activeQuestion + 1}
+                                state={this.props.answerState}
+                            />
+                        </>
                     )}
                 </div>
             </div>
